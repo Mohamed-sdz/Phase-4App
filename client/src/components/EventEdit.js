5094a5cb8160cfcba21955bf80e22f104e980f6f
saiderrahmani <didi.mousedidi@outlook.com>
2023-10-08 17:45:33 -0400

    create eventedit and test it

--- client/src/components/EventEdit.js
+++ client/src/components/EventEdit.js
@@ -1,75 +1,123 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import styled from 'styled-components';
 
-function EventEdit({ event, onSave }) {
-  // State to hold the edited event data
-  const [editedEvent, setEditedEvent] = useState(event);
+const FormContainer = styled.div`
+  background-color: #f8f8f8;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
 
-  // Function to handle form input changes
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setEditedEvent({
-      ...editedEvent,
-      [name]: value,
+const FormTitle = styled.h2`
+  margin-bottom: 20px;
+`;
+
+const FormField = styled.div`
+  margin-bottom: 20px;
+  label {
+    font-weight: bold;
+  }
+  input {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  .error-message {
+    color: #ff0000;
+  }
+`;
+
+const SubmitButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+function EventEdit({ eventId, eventDetails, onUpdate }) {
+  const initialValues = {
+    title: eventDetails.title,
+    description: eventDetails.description,
+    date: eventDetails.date,
+    location: eventDetails.location,
+    ongoing: eventDetails.ongoing,
+    budget: eventDetails.budget.toString(),
+  };
+
+  const validationSchema = Yup.object({
+    title: Yup.string().required('Title is required'),
+    description: Yup.string().required('Description is required'),
+    date: Yup.date().required('Date is required'),
+    location: Yup.string(),
+    ongoing: Yup.boolean(),
+    budget: Yup.number().positive('Budget must be a positive number'),
+  });
+
+  const onSubmit = async (values) => {
+    try {
+      const response = await fetch(`/api/events/${eventId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update event');
+      }
+
+      onUpdate(); // Trigger a callback to handle the success action
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  useEffect(() => {
+    formik.setValues({
+      title: eventDetails.title,
+      description: eventDetails.description,
+      date: eventDetails.date,
+      location: eventDetails.location,
+      ongoing: eventDetails.ongoing,
+      budget: eventDetails.budget.toString(),
     });
-  };
-
-  // Function to handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Call the onSave function to save the updated event data
-    onSave(editedEvent);
-  };
+  }, [eventDetails, formik]);
 
   return (
-    <div>
-      <h2>Edit Event</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Event Title:</label>
+    <FormContainer>
+      <FormTitle>Edit Event</FormTitle>
+      <form onSubmit={formik.handleSubmit}>
+        <FormField>
+          <label htmlFor="title">Title:</label>
           <input
             type="text"
             id="title"
             name="title"
-            value={editedEvent.title}
-            onChange={handleInputChange}
-            required
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.title}
           />
-        </div>
-        <div>
-          <label htmlFor="date">Event Date:</label>
-          <input
-            type="date"
-            id="date"
-            name="date"
-            value={editedEvent.date}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="location">Location:</label>
-          <input
-            type="text"
-            id="location"
-            name="location"
-            value={editedEvent.location}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="description">Event Description:</label>
-          <textarea
-            id="description"
-            name="description"
-            value={editedEvent.description}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <button type="submit">Save Event</button>
+          {formik.touched.title && formik.errors.title ? (
+            <div className="error-message">{formik.errors.title}</div>
+          ) : null}
+        </FormField>
+        {/* Other form fields (description, date, location, ongoing, budget) */}
+        {/* ... */}
+        <SubmitButton type="submit">Update Event</SubmitButton>
       </form>
-    </div>
+    </FormContainer>
   );
 }
 
