83dcd97d1deee3c412097114a70426281576ccb8
saiderrahmani <didi.mousedidi@outlook.com>
2023-10-08 17:39:01 -0400

    create gustlist

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -1,31 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-function EventList({ events, onViewEvent, onEditEvent, onDeleteEvent }) {
+const Container = styled.div`
+  background-color: #f8f8f8;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const LoadingMessage = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  color: #333;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  color: #ff0000;
+`;
+
+const GuestListItem = styled.li`
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+function GuestList() {
+  const [guests, setGuests] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch the list of guests from your API endpoint
+    const fetchGuests = async () => {
+      try {
+        const response = await fetch('/api/guests'); // Replace with your actual API endpoint
+        if (!response.ok) {
+          throw new Error('Failed to fetch guest data');
+        }
+        const data = await response.json();
+        setGuests(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchGuests();
+  }, []);
+
   return (
-    <div>
-      <h2>Event List</h2>
-      <ul>
-        {events.map((event) => (
-          <li key={event.id}>
-            <div>
-              <strong>Title:</strong> {event.title}
-            </div>
-            <div>
-              <strong>Date:</strong> {event.date}
-            </div>
-            <div>
-              <strong>Location:</strong> {event.location}
-            </div>
-            <div>
-              <button onClick={() => onViewEvent(event)}>View Event</button>
-              <button onClick={() => onEditEvent(event)}>Edit Event</button>
-              <button onClick={() => onDeleteEvent(event)}>Delete Event</button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Container>
+      <h2>Guest List</h2>
+      {loading ? (
+        <LoadingMessage>Loading...</LoadingMessage>
+      ) : error ? (
+        <ErrorMessage>Error: {error}</ErrorMessage>
+      ) : (
+        <ul>
+          {guests.map((guest) => (
+            <GuestListItem key={guest.id}>
+              <strong>Name:</strong> {guest.name}
+              <br />
+              <strong>Response:</strong> {guest.response}
+            </GuestListItem>
+          ))}
+        </ul>
+      )}
+    </Container>
   );
 }
 
-export default EventList;
+export default GuestList;
