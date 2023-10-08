7527b64fc331eb67efa0aa0b180465337e3715f4
saiderrahmani <didi.mousedidi@outlook.com>
2023-10-08 17:41:37 -0400

    update eventdetail

--- client/src/components/EventDetail.js
+++ client/src/components/EventDetail.js
@@ -1,6 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-function EventDetail({ eventId }) {
+const EventDetailsContainer = styled.div`
+  background-color: #f8f8f8;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const LoadingMessage = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  color: #333;
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  color: #ff0000;
+`;
+
+const EventInfo = styled.div`
+  margin-top: 20px;
+`;
+
+const EventDetail = ({ eventId }) => {
   const [eventDetails, setEventDetails] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -26,23 +50,25 @@ function EventDetail({ eventId }) {
   }, [eventId]);
 
   if (loading) {
-    return <p>Loading event details...</p>;
+    return <LoadingMessage>Loading event details...</LoadingMessage>;
   }
 
   if (!eventDetails) {
-    return <p>Error: Event not found</p>;
+    return <ErrorMessage>Error: Event not found</ErrorMessage>;
   }
 
   return (
-    <div>
+    <EventDetailsContainer>
       <h2>Event Details</h2>
-      <p><strong>Title:</strong> {eventDetails.title}</p>
-      <p><strong>Description:</strong> {eventDetails.description}</p>
-      <p><strong>Date:</strong> {new Date(eventDetails.date).toLocaleDateString()}</p>
-      <p><strong>Location:</strong> {eventDetails.location}</p>
-      <p><strong>Ongoing:</strong> {eventDetails.ongoing ? 'Yes' : 'No'}</p>
-      <p><strong>Budget:</strong> {eventDetails.budget}</p>
-    </div>
+      <EventInfo>
+        <p><strong>Title:</strong> {eventDetails.title}</p>
+        <p><strong>Description:</strong> {eventDetails.description}</p>
+        <p><strong>Date:</strong> {new Date(eventDetails.date).toLocaleDateString()}</p>
+        <p><strong>Location:</strong> {eventDetails.location}</p>
+        <p><strong>Ongoing:</strong> {eventDetails.ongoing ? 'Yes' : 'No'}</p>
+        <p><strong>Budget:</strong> {eventDetails.budget}</p>
+      </EventInfo>
+    </EventDetailsContainer>
   );
 }
 
